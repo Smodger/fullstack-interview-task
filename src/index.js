934b32dfdb53d04967ff999643e16d3a09888c3a
Chris Norman <chris.norman@lakeland.co.uk>
2022-11-21 16:37:55 +0000

    added investments end point to get all investments. added csvWriter package for ease of writing. Compiled data to be sent to end point. Still to do: refactor to replace request package, create csv, send to endpoint

--- src/index.js
+++ src/index.js
@@ -4,20 +4,89 @@ const bodyParser = require("body-parser")
 const config = require("config")
 const request = require("request")
 
+const createCsvWriter = require('csv-writer').createObjectCsvWriter
+
 const app = express()
 
 app.use(bodyParser.json({limit: "10mb"}))
 
-app.get("/investments/:id", async (req, res) => {
-  const {id} = req.params
-  request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
-    if (e) {
-      console.error(e)
-      res.send(500)
-    } else {
-      res.send(investments)
+const csvWriter = createCsvWriter({
+    path: '.csv',
+    header: [
+        {id: 'user', title: 'User'},
+        {id: 'firstName', title: 'First Name'},
+        {id: 'surname', title: 'Surname'},
+        {id: 'date', title: 'Date'},
+        {id: 'holding', title: 'Holding'},
+        {id: 'value', title: 'Value'},
+    ]
+})  
+
+// As we only have 3 companies in this example I am using this hash. Otherwise I'd look to have a more dynamic look up (see function 'getCompanyNames')
+const companyHash = {
+    1 : "The Big Investment Company",
+    2 : "The Small Investment Company",
+    3 : "Capital Investments"
+}
+
+const compileReportData = (data) => {
+
+    let csvData = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const person = data[i];
+
+        for (let j = 0; j < person.holdings.length; j++) {
+            const holding = person.holdings[j]
+            csvData.push({
+                user : person.userId,
+                firstName : person.firstName,
+                surname : person.lastName,
+                date : person.date,
+                holding : companyHash[holding.id],
+                value : person.investmentTotal * holding.investmentPercentage
+            })
+        } 
     }
-  })
+
+    console.log(csvData);
+} 
+
+// const getCompanyNames = (holdings) => {
+//     holdings.map((holding) => {
+//         axios.get(`http://localhost:8082/companies/${holding.id}`, (e, r, name) => {
+//             if(e) {
+//                 console.error(e)
+//                 res.send(500)
+//             }else {
+//                 console.log(JSON.parse(name))
+//             }
+//         })
+//     })
+// }
+
+app.get("/investments/:id", async (req, res) => {
+    const {id} = req.params
+    request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
+        if (e) {
+            console.error(e)
+            res.send(500)
+        } else {
+            res.send(investments)
+        }
+    })
+})
+
+app.get("/investments/", async (req, res) => {
+    request.get(`${config.investmentsServiceUrl}/investments/`, (e, r, investments) => {
+        if (e) {
+            console.error(e)
+            res.send(500)
+        } else {
+            res.send(investments)
+            compileReportData(JSON.parse(investments))
+        }
+    })
 })
 
 app.listen(config.port, (err) => {
